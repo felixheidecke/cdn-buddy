e661858265b70afb750c0a98601374a1bd6de89a
Felix Heidecke <felix@heidecke.me>
2020-03-11 18:17:59 +0100

    Global configuration

--- buddy.js
+++ buddy.js
@@ -1,95 +1,121 @@
 class buddy {
 
   constructor () {
-    this._baseUrl = ''
-    this._paths = {}
+    this.__ns_status = '__CDN_BUDDY_STATUS'
+    this.__ns_queue  = '__CDN_BUDDY_QUEUE'
+    this.__ns_config = '__CDN_BUDDY_CONFIG'
+
+    if (!window[this.__ns_queue]) { window[this.__ns_queue] = [] }
+    if (!window[this.__ns_status]) { window[this.__ns_status] = {} }
+    if (!window[this.__ns_config]) {
+      window[this.__ns_config] = {
+        baseUrl: null,
+        paths: {}
+      }
+    }
   }
 
   set config (config) {
-    this._baseUrl = (config.baseUrl) || ''
-    this._paths = (config.paths) || {}
-  }
-
-  set paths (path) {
-    if (typeof path !== 'object')
+    if (typeof config !== 'object')
       throw Error('path setter expecting object')
 
-    Object.assign(this._paths, path)
+    window[this.__ns_config] = config
   }
 
-  get path () {
-    return this._paths
+  get queue () {
+    return window[this.__ns_queue]
   }
 
-  _createLinkElement (uri) {
+  get config () {
+    return window[this.__ns_config]
+  }
+
+  get status () {
+    return window[this.__ns_status]
+  }
+
+  _createLinkElement (path) {
     let el = document.createElement('link')
     el.setAttribute('rel', 'stylesheet')
-    el.setAttribute('href', uri)
+    el.setAttribute('data-cdn-buddy', true)
+    el.setAttribute('href', path)
     
     return el
   }
 
-  _createScriptElement (uri) {
+  _createScriptElement (path) {
     let el = document.createElement('script')
-    el.setAttribute('src', uri)
+    el.setAttribute('src', path)
+    el.setAttribute('data-cdn-buddy', true)
 
     return el
   }
 
-  _insert (path) {
-    let lib = (this._paths[path]) || path
-    let uri = [this._baseUrl, lib].join('/')
+  _createPath (path) {
+
+    const baseUrl = window[this.__ns_config].baseUrl
+    const paths = window[this.__ns_config].paths
+
+    if (path.search(/(http(s)?):\/\//) === 0) {
+      return path
+    }
+
+    if (paths[path]) {
+      return baseUrl + '/' + paths[path]
+    }
+    
+    return baseUrl + '/' + path
+  }
+
+  _insert (library) {
+    let path = this._createPath(library)
 
     return new Promise((resolve, reject) => {
-        let id = '__buddy__' + btoa(lib)
         let el = ''
 
-        if (document.getElementById(id) !== null) {
-            resolve()
-            return
-        }
-
-        if (lib.search(/\.js$/) !== -1) {
-          el = this._createScriptElement(uri)
-          document.body.appendChild(el);
-        }
-        else if (lib.search(/\.css$/) !== -1) {
-          el = this._createLinkElement(uri)
-          document.head.appendChild(el);
-        }
-        else {
-          reject('Something went wrong')
+        if (window[this.__ns_status][path] === 'done') {
+          resolve()
           return
         }
 
-        el.setAttribute('id', id)
+        if (path.search(/\.js$/) !== -1) {
+          el = this._createScriptElement(path)
+          document.body.appendChild(el);
+        }
+        else if (path.search(/\.css$/) !== -1) {
+          el = this._createLinkElement(path)
+          document.head.appendChild(el);
+        }
+        else {
+          el = this._createScriptElement(path + '.js')
+          document.body.appendChild(el);
+        }
 
-        el.addEventListener('load', function() {
-            console.info(`😀 Buddy Loaded: ${uri}`)
+        el.addEventListener('load', () => {
+            console.info(`😀 Buddy Loaded: ${path}`)
+            window[this.__ns_status][path] = 'done'
             resolve()
         });
 
-        el.addEventListener('error', function() {
-            console.error(`😟 Buddy could not load: ${uri}`)
-            reject()
-            return
+        el.addEventListener('error', error => {
+            console.error(`😟 Buddy could not load: ${path}`)
+            window[this.__ns_status][path] = 'error'
+            reject(error)
         })
-
     });
   }
 
   require (lib) {
-    if (typeof lib === "string") {
-      return this._insert(lib);
-    }
-    else if (typeof lib === "object") {
-        let libs = [];
-        lib.forEach(library => {
-            libs.push(this._insert(library))
-        })
-        
-        return Promise.all(libs);
-    }
+    lib.forEach(library => {
+      library = this._createPath(library)
+
+      if (!window[this.__ns_status][library]) {
+        window[this.__ns_status][library] = 'loading'
+        window[this.__ns_queue].push(this._insert(library))
+      }
+    })
+
+    return Promise.all(window[this.__ns_queue]);
   }
 }
 
